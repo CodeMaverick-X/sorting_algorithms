6dd53f8e1859d32289664a035aee022e78b6bfd6
reinhardcode <reinhard2057.com>
2022-10-09 22:42:07 +0000

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * selection_sort - slection sort for arrays
+ * @array: array to be sorted
+ * @size: size of the array to be sorted
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min_pos, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_pos = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_pos])
+				min_pos = j;
+		}
+		if (min_pos != i)
+		{
+			temp = array[i];
+			array[i] = array[min_pos];
+			array[min_pos] = temp;
+		}
+		print_array(array, size);
+	}
+}
