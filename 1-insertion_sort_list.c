f6e458d76b68b646262b2ce116234c3b98d77737
reinhardcode <reinhard2057.com>
2022-10-09 19:35:56 +0000

    finally gotten insertion sort to work

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list using
+ * the insertion sort algorithm
+ * @list: double pointer to head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+	listint_t *i = NULL;
+	listint_t *hold = NULL;
+	listint_t *key = NULL;
+	listint_t *j = NULL;
+
+	if (*list == NULL)
+		return;
+
+
+	i = (*list)->next;
+
+	while (i != NULL)
+	{
+		key = i;
+		i = i->next;
+		j = key->prev;
+		hold = key->next;
+
+		while (j != NULL && (j->n) > (key->n))
+		{
+			j->next = hold;
+			if (hold)
+				(hold)->prev = j;
+			hold = j;
+			j = j->prev;	
+		}
+		key->prev = j;
+		if (j)
+			j->next = key;
+		else
+			*list = key;
+
+		key->next = hold;
+		if (hold)
+			hold->prev = key;
+	
+		print_list(*list);
+	}
+}
--- sort.h
+++ sort.h
@@ -6,11 +6,26 @@
 #include <string.h>
 
 
-/* prototype for test functions used by checker */
-void print_array(const int *array, size_t size);
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
 
 /* prototypes for our functions */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
+/* prototype for test functions used by checker */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 #endif
