7b5e20bbb3daa08ea215d51d61b132ae526847d7
coderboy-exe <coderboy.exe@gmail.com>
2022-10-11 12:52:45 +0000

    betty compliant, handle free for no count array

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,8 +10,7 @@
 
 void counting_sort(int *array, size_t size)
 {
-	int *new_arr, *count;
-	int i, max;
+	int *new_arr, *count, i, max;
 
 	new_arr = malloc(sizeof(int) * size);
 	if (!array)
@@ -19,44 +18,33 @@ void counting_sort(int *array, size_t size)
 		free(new_arr);
 		return;
 	}
-
 	max = array[0];
 	for (i = 0; i < (int)size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
 	}
-
 	count = malloc(sizeof(int) * (max + 1));
 	for (i = 0; i <= max; ++i)
 		count[i] = 0;
 
 	if (!count)
 	{
-		free(new_arr);
+		free(count);
 		return;
 	}
-
-	/*Store the count of each element*/
 	for (i = 0; i < (int)size; i++)
 		count[array[i]]++;
 
-	/*Store the cumulative count of the array*/
 	for (i = 1; i <= max; i++)
 		count[i] += count[i - 1];
-
-	/*Print the counting array*/
 	print_array(count, max + 1);
 
-	/*Find the index of each element of the original array in the
-	count array and place them in the new_arr*/
-	for(i = size - 1; i >= 0; i--)
+	for (i = size - 1; i >= 0; i--)
 	{
 		new_arr[count[array[i]] - 1] = array[i];
 		count[array[i]]--;
 	}
-
-	/*Copy sorted elements into original array*/
 	for (i = 0; i < (int)size; ++i)
 		array[i] = new_arr[i];
 
